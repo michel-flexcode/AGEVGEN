0c9ca2e7a8f834b042f77eb88e1915fcb020a383
MichelCode <michelseocode.com>
2024-02-14 12:31:07 +0100

    features-mich-7 course

--- resources/js/Pages/Courses/Edit.vue
+++ resources/js/Pages/Courses/Edit.vue
@@ -9,33 +9,16 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
 
 const props = defineProps({
-    section: Object,
-    courses: Array, // Passer les courses disponibles comme prop
-    course: Object, // Passer les courses disponibles comme prop
-    allCourses: Array,
-    allCourses: Object,
+    course: Object,
 });
 
 const form = useForm({
     _method: "PUT",
-    Name: props.section.Name,
-    course: props.courses.length > 0 ? props.courses[0]?.id : null,
-    course2: props.courses.length > 0 ? props.courses[1]?.id : null,
-    course3: props.courses.length > 0 ? props.courses[2]?.id : null,
+    name: props.course.name,
 });
 
-console.log("Nombre de courses:", props.courses.length);
-if (props.courses.length > 0) {
-    console.log("IDs des courses associées à l'étudiant:");
-    props.courses.forEach((course) => {
-        console.log(course.course_id);
-    });
-} else {
-    console.log("Aucune course associée à l'étudiant.");
-}
-
 const sendForm = () => {
-    form.put(route("sections.update", props.section), {
+    form.put(route("courses.update", props.course), {
         preserveScroll: true,
         onSuccess: () => {
             form.reset();
@@ -45,25 +28,24 @@ const sendForm = () => {
 </script>
 
 <template>
-    <AppLayout title="Sections">
+    <AppLayout title="Courses">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Modifier une section
+                Modifier un cours
             </h2>
         </template>
 
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             <FormSection @submitted="sendForm">
-                <template #title> Modification d'une section </template>
+                <template #title> Modification d'un cours </template>
 
                 <template #description>
-                    Les sections seront reprises sur le formulaire pour les
-                    étudiants
+                    Les cours seront repris sur le formulaire pour les sections
                 </template>
 
                 <template #form>
                     <div class="col-span-6 sm:col-span-4">
-                        <InputLabel for="name" value="Name" />
+                        <InputLabel for="name" value="Nom" />
                         <TextInput
                             id="name"
                             type="text"
@@ -72,103 +54,18 @@ const sendForm = () => {
                         />
                         <InputError :message="form.errors.name" class="mt-2" />
                     </div>
-
-                    <div class="col-span-6 sm:col-span-4">
-                        <InputLabel for="course" value="Course" />
-                        <select
-                            id="course"
-                            class="mt-1 block w-full"
-                            v-model="form.course"
-                        >
-                            <option value="">Select a course</option>
-                            <!-- Boucle sur toutes les courses pour la sélection -->
-                            <option
-                                v-for="course in allCourses"
-                                :key="course.id"
-                                :value="course.id"
-                                :selected="
-                                    props.courses.length > 0 &&
-                                    course.id === props.courses[0].course_id
-                                "
-                            >
-                                {{ course.id }}
-                                {{ course.name }}
-                            </option>
-                        </select>
-                        <InputError
-                            :message="form.errors.course"
-                            class="mt-2"
-                        />
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-4">
-                        <InputLabel for="course2" value="Course 2" />
-                        <select
-                            id="course2"
-                            class="mt-1 block w-full"
-                            v-model="form.course2"
-                        >
-                            <option value="">Select a course</option>
-                            <!-- Boucle sur toutes les courses pour la sélection -->
-                            <option
-                                v-for="course in allCourses"
-                                :key="course.id"
-                                :value="course.id"
-                                :selected="
-                                    props.courses.length > 1 &&
-                                    props.courses[1] !== undefined &&
-                                    course.id === props.courses[1].course_id
-                                "
-                            >
-                                {{ course.id }}
-                                {{ course.Name }}
-                            </option>
-                        </select>
-                        <InputError
-                            :message="form.errors.course2"
-                            class="mt-2"
-                        />
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-4">
-                        <InputLabel for="course3" value="Course 3" />
-                        <select
-                            id="course3"
-                            class="mt-1 block w-full"
-                            v-model="form.course3"
-                        >
-                            <option value="">Select a course</option>
-                            <!-- Boucle sur toutes les courses pour la sélection -->
-                            <option
-                                v-for="course in allCourses"
-                                :key="course.id"
-                                :value="course.id"
-                                :selected="
-                                    props.courses.length > 2 &&
-                                    course.id === props.courses[2].course_id
-                                "
-                            >
-                                {{ course.id }}
-                                {{ course.Name }}
-                            </option>
-                        </select>
-                        <InputError
-                            :message="form.errors.course3"
-                            class="mt-2"
-                        />
-                    </div>
                 </template>
 
                 <template #actions>
                     <ActionMessage :on="form.recentlySuccessful" class="me-3">
-                        Saved.
+                        Sauvegardé.
                     </ActionMessage>
 
                     <PrimaryButton
                         :class="{ 'opacity-25': form.processing }"
                         :disabled="form.processing"
                     >
-                        Save
+                        Sauvegarder
                     </PrimaryButton>
                 </template>
             </FormSection>
--- resources/js/Pages/Courses/Index.vue
+++ resources/js/Pages/Courses/Index.vue
@@ -7,62 +7,56 @@ import DialogModal from "@/Components/DialogModal.vue";
 import { ref } from "vue";
 
 const props = defineProps({
-    sections: Array,
-    section: Object,
+    courses: Array,
+    course: Object,
 });
 
 //ajout bloc delete
-const confirmingSectionDeletion = ref(false);
-const sectionIdToDelete = ref(null);
-const formDeleteSection = useForm("delete", {});
+const confirmingCourseDeletion = ref(false);
+const courseIdToDelete = ref(null);
+const formDeleteCourse = useForm("delete", {});
 
-const confirmSectionDeletion = (id) => {
-    sectionIdToDelete.value = id;
-    confirmingSectionDeletion.value = true;
+const confirmCourseDeletion = (id) => {
+    courseIdToDelete.value = id;
+    confirmingCourseDeletion.value = true;
 };
 
-const deleteSection = () => {
-    formDeleteSection.delete(
-        route("sections.destroy", sectionIdToDelete.value),
-        {
-            preserveScroll: true,
-            onSuccess: () => {
-                confirmingSectionDeletion.value = false;
-            },
-        }
-    );
+const deleteCourse = () => {
+    formDeleteCourse.delete(route("courses.destroy", courseIdToDelete.value), {
+        preserveScroll: true,
+        onSuccess: () => {
+            confirmingCourseDeletion.value = false;
+        },
+    });
 };
 
 const closeModal = () => {
-    confirmingSectionDeletion.value = false;
+    confirmingCourseDeletion.value = false;
 };
-
-console.log(courses); // Ajout du console.log pour afficher le contenu de la variable courses
 </script>
 
 <template>
     <AppLayout>
         <div class="bg-gray-100 py-6">
             <header class="text-center mb-8">
-                <h2 class="text-2xl font-semibold text-gray-800">Sections</h2>
+                <h2 class="text-2xl font-semibold text-gray-800">Cours</h2>
             </header>
 
             <div class="max-w-4xl mx-auto">
                 <ul
                     class="bg-white rounded-lg shadow overflow-hidden divide-y divide-gray-200"
                 >
-                    <li v-for="section in sections" :key="section.id">
+                    <li v-for="course in courses" :key="course.id">
                         <div class="px-4 py-4 sm:px-6">
                             <div class="flex items-center justify-between">
-                                <Link :href="route('sections.edit', section)">
-                                    {{ section.name }}
-                                    {{ section.surname }}
-                                    {{ section.email }}
+                                <Link :href="route('courses.edit', course)">
+                                    {{ course.id }} -
+                                    {{ course.name }}
                                 </Link>
                                 <div class="space-x-4">
                                     <button
                                         @click.prevent="
-                                            confirmSectionDeletion(section.id)
+                                            confirmCourseDeletion(course.id)
                                         "
                                         class="px-4 py-2 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-800 focus:ring-opacity-50 transition ease-in-out duration-150"
                                     >
@@ -73,7 +67,7 @@ console.log(courses); // Ajout du console.log pour afficher le contenu de la var
                                     >
                                         <Link
                                             :href="
-                                                route('sections.edit', section)
+                                                route('courses.edit', course)
                                             "
                                         >
                                             Edit
@@ -88,20 +82,20 @@ console.log(courses); // Ajout du console.log pour afficher le contenu de la var
                 <div class="mt-6 text-center">
                     <Link
                         class="text-white bg-blue-800 px-4 py-2 font-semibold rounded-lg hover:bg-blue-700 hover:text-white transition duration-300 ease-in-out"
-                        :href="route('sections.create')"
+                        :href="route('courses.create')"
                     >
-                        Créer une nouvelle section
+                        Créer un nouveau cours
                     </Link>
                 </div>
             </div>
         </div>
     </AppLayout>
 
-    <DialogModal :show="confirmingSectionDeletion" @close="closeModal">
-        <template #title> Supprimer la section </template>
+    <DialogModal :show="confirmingCourseDeletion" @close="closeModal">
+        <template #title> Supprimer le cours </template>
 
         <template #content>
-            Êtes-vous sûr de vouloir supprimer cette section ? Cette action est
+            Êtes-vous sûr de vouloir supprimer ce cours ? Cette action est
             irréversible.
         </template>
 
@@ -110,9 +104,9 @@ console.log(courses); // Ajout du console.log pour afficher le contenu de la var
 
             <DangerButton
                 class="ms-3"
-                :class="{ 'opacity-25': confirmingSectionDeletion.processing }"
-                :disabled="confirmingSectionDeletion.processing"
-                @click="deleteSection"
+                :class="{ 'opacity-25': confirmingCourseDeletion.processing }"
+                :disabled="confirmingCourseDeletion.processing"
+                @click="deleteCourse"
             >
                 Supprimer
             </DangerButton>
